fbe1387a224dfe4d23bfb7b1800ac8366dc30342
Victor Hugo <vhs@certi.org.br>
2021-10-19 00:07:28 -0300

    header

--- nubank-app/src/app/home/home.page.ts
+++ nubank-app/src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+//https://www.youtube.com/watch?v=5LtVN_j3tmI
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,6 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  public options: Array<any> = [
+    { icon: 'person-add-outline', text: 'Indicados amigos'},
+    { icon: 'phone-portrait-outline', text:'recarga de celular' },
+    { icon: 'wallet-outline', text: 'Depositar' },
+    { icon: 'oprions-outline', text: 'Ajustar limite' },
+    { icon: 'help-circle-outline', text: 'Me ajuda' },
+    { icon: 'barcode-outline', text: 'Pagar' },
+    { icon: 'lock-apen-outline', text: 'Bloquear cartao' },
+    { icon: 'card-aoutline', text: 'Cartao virtual' }
+  ]
 
   constructor() {}
 
